f016d2495ff59d8ebe7958fc5ab6b50929af443f
Vikhyath Choradia <famous.vikhyath@gmail.com>
2026-02-22 11:31:26 +0100

    Add logo

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const langPicker = document.getElementById('language-picker');
-    
-    langPicker.addEventListener('change', (e) => {
-        const selectedLang = e.target.value;
-        const translatableElements = document.querySelectorAll('[data-en]');
-        
-        translatableElements.forEach(el => {
-            el.textContent = el.getAttribute(`data-${selectedLang}`);
-        });
+document.addEventListener("DOMContentLoaded", () => {
+  const langPicker = document.getElementById("language-picker");
+
+  langPicker.addEventListener("change", (e) => {
+    const selectedLang = e.target.value;
+    const translatableElements = document.querySelectorAll("[data-en]");
+
+    translatableElements.forEach((el) => {
+      el.textContent = el.getAttribute(`data-${selectedLang}`);
     });
+    const logo = document.getElementsByClassName("logo-wrapper")[0];
+    Array.from(logo.classList).forEach((className) => {
+      if (className.startsWith("lang-")) {
+        logo.classList.remove(className);
+      }
+    });
+    logo.classList.add(`lang-${selectedLang}`);
+  });
 });
